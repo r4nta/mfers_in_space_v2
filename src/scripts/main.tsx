6e7a958f4687ef51d7a3b9cee49ebfb367d9232c
r4nta <32648170+r4nta@users.noreply.github.com>
2022-02-22 07:44:17 +0100

    Add files via upload

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -2,7 +2,8 @@ import '../styles/main.scss';
 
 import ReactDOM from 'react-dom';
 import Dapp from './react/Dapp';
+import Intro from './react/Intro';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  ReactDOM.render(<Dapp />, document.getElementById('minting-dapp'));
+  ReactDOM.render(<><Intro /><Dapp /></>, document.getElementById('minting-dapp'));
 });
